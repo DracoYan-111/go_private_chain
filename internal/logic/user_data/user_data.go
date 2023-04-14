ac0cc2480abcc3f80b0ad7a0b0325e4971f283d0
LongYan <2944972991@qq.com>
2023-04-14 12:09:55 +0800

    Reduce constraints for stress testing

--- internal/logic/user_data/user_data.go
+++ internal/logic/user_data/user_data.go
@@ -44,9 +44,7 @@ func (s *sUserData) CreateUserAddress(ctx context.Context, req string) (string, 
 	// 检查opcode是否已经存在
 	opcode := utility.RandomNumber()
 	dbUserAddress, err := dao.UserData.Ctx(ctx).Where("opcode", opcode).All()
-	dbUserNick, err := dao.UserData.Ctx(ctx).Where("user_nick", aesDecrypt).All()
-
-	if err != nil || dbUserAddress.Len() != 0 || dbUserNick.Len() != 0 {
+	if err != nil || dbUserAddress.Len() != 0 {
 		return "", fmt.Errorf("检查重复失败(CreateUserAddress):%s", err)
 	}
 
