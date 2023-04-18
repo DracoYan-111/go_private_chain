b0676d1148799b4748e862d33ec550fa06483798
LongYan <2944972991@qq.com>
2023-04-18 14:29:52 +0800

    update GetUserCollectionReq

--- api/v1/get_user_data.go
+++ api/v1/get_user_data.go
@@ -17,11 +17,11 @@ type GetUserAddressRes struct {
 }
 
 type GetUserCollectionReq struct {
-	g.Meta   `path:"/user/get-user-address" method:"get" tags:"GetUserAddress" summary:"查询用户的账户地址"`
+	g.Meta   `path:"/user/get-user-collection" method:"get" tags:"GetUserCollections" summary:"查询用户的所有藏品"`
 	Nickname string `v:"required" summary:"用户昵称"`
 }
 
 type GetUserCollectionRes struct {
 	OK             bool                     `v:"required" summary:"任务状态"`
-	UserCollection []entity.UserCollections `v:"required" summary:"用户对应的账户地址"`
+	UserCollection []entity.UserCollections `v:"required" summary:"用户对应的藏品详情"`
 }
